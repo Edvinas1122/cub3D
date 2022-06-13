ed4aa789298b586e686462867a7e68d829b052c0
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-06-13 22:36:32 +0200

    moved floor and ceiling rendering to the beginning

--- src/display/draw_vertical_line.c
+++ src/display/draw_vertical_line.c
@@ -16,16 +16,16 @@ static void	set_wall_data(t_wall *wall, t_raycast *raycast)
 		wall->end = SCREEN_HEIGHT - 1;
 }
 
-static double	get_dim_factor(int y)
-{
-	double	factor;
-	int		delta;
+// static double	get_dim_factor(int y)
+// {
+// 	double	factor;
+// 	int		delta;
 
-	delta = abs(y - (SCREEN_HEIGHT / 2));
-	factor = 1 - ((double)delta / (SCREEN_HEIGHT/2));
-	factor = factor*factor*factor*factor;
-	return (1-factor);
-}
+// 	delta = abs(y - (SCREEN_HEIGHT / 2));
+// 	factor = 1 - ((double)delta / (SCREEN_HEIGHT/2));
+// 	factor = factor*factor*factor*factor;
+// 	return (1-factor);
+// }
 
 /*
 	Draws vertical pixel line using raycast data
@@ -34,15 +34,15 @@ static double	get_dim_factor(int y)
 void	draw_vertical_line(t_data *data, t_raycast *raycast)
 {
 	t_wall	wall;
-	double	dimfactor;
+	// double	dimfactor;
 
 	set_wall_data(&wall, raycast);
-	while (wall.y < wall.start)
-	{
-		dimfactor = get_dim_factor(wall.y);
-		pixel_put(data->video.img_matrix, dim_color(data->map.ceiling, dimfactor), raycast->v_line_ct, wall.y);
-		wall.y++;
-	}
+	// while (wall.y < wall.start)
+	// {
+	// 	dimfactor = get_dim_factor(wall.y);
+	// 	pixel_put(data->video.img_matrix, dim_color(data->map.ceiling, dimfactor), raycast->v_line_ct, wall.y);
+	// 	wall.y++;
+	// }
 	// t_color tmpcol;
 	// tmpcol.a = 0;
 	// tmpcol.r = 120;
@@ -53,11 +53,12 @@ void	draw_vertical_line(t_data *data, t_raycast *raycast)
 	// 	pixel_put(data->video.img_matrix, tmpcol, raycast->v_line_ct, wall.y);
 	// 	wall.y++;
 	// }
+	wall.y = wall.start;
 	draw_wall_line(data, raycast, &wall);
-	while (wall.y < SCREEN_HEIGHT)
-	{
-		dimfactor = get_dim_factor(wall.y);
-		pixel_put(data->video.img_matrix, dim_color(data->map.floor, dimfactor), raycast->v_line_ct, wall.y);
-		wall.y++;
-	}
+	// while (wall.y < SCREEN_HEIGHT)
+	// {
+	// 	dimfactor = get_dim_factor(wall.y);
+	// 	pixel_put(data->video.img_matrix, dim_color(data->map.floor, dimfactor), raycast->v_line_ct, wall.y);
+	// 	wall.y++;
+	// }
 }
